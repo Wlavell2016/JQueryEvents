5fb51c066ea633d6cb88051670afce8ad5a78c74
Wlavell2016 <william.lavell@gmail.com>
2016-11-23 17:39:59 -0500

    finished part 1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
+$(function() {
+  $('button').on('click', function(eventObject) {
+      console.log('button was clicked');
+      $('button' ).toggleClass('off');
+      $('button' ).toggleClass('on');
+      $('body' ).toggleClass('light');
+      $('body' ).toggleClass('dark');
 
-
+      if ($('button').hasClass('off')) {
+          $('h1').text('lights turned off');
+      }  else {
+          $('h1').text('lights turned on');
+      }
+});
+});
